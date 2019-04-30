3e1cf5d1179bc3b5f55ba10c2835080ade89399b
Justin Perez <jperez.devkaizen@gmail.com>
2019-04-30 15:56:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays(string) {
   return "Happy holidays!"
 }
 
-function happyHolidayTo(name) {
-  return `Happy holidays, ` + `${name}` + `!`;
-}
+function happyHolidaysTo (name) {
+  return "Happy holidays, " + `${name}` + "!";	  return `Happy holidays, ${name}!`;
+}	}
